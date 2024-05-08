0a60c9606f33c84be6058f2ec22036cbe93b8bf6
Makeshwar K <138497410+Makeshwar@users.noreply.github.com>
2024-05-08 15:59:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NumberFetcher from './NumberFetcher';
+import './corsConfig';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Average Calculator</h1>
+      <NumberFetcher numberType="even" /> {/* Fetch even numbers */}
+      <NumberFetcher numberType="primes" /> {/* Fetch prime numbers */}
+      <NumberFetcher numberType="fibo" /> {/* Fetch Fibonacci numbers */}
+      <NumberFetcher numberType="rand" /> {/* Fetch random numbers */}
+    </div>
+  );
+};
+
+export default App;
